6e706bd56ca044fa027498298cb0e819dda0523c
unknown <cshields10>
2019-07-17 16:00:44 -0700

    almost finished multibar

---
+++ VMSpc/Panels/ParamContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace VMSpc.Panels
+{
+    public class ParamPresenter
+    {
+        protected bool UseMetric;
+        protected VParameter parameter;
+        public double lastValue;
+        public double currentValue => ((UseMetric) ? parameter.LastValue : parameter.LastMetricValue);
+        public string valueAsString => (currentValue + ((UseMetric) ? parameter.Unit : parameter.MetricUnit));
+
+        public ParamPresenter(ushort pid, bool UseMetric)
+        {
+            this.UseMetric = UseMetric;
+        }
+
+    }
+}
--- VMSpc/Panels/VMultiBar.cs
+++ VMSpc/Panels/VMultiBar.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using VMSpc.CustomComponents;
 using VMSpc.DevHelpers;
 using VMSpc.DlgWindows;
 using VMSpc.XmlFileManagers;
@@ -18,35 +19,12 @@ namespace VMSpc.Panels
 {
     class VMultiBar : VPanel
     {
-        protected TextBlock titleText;
-        protected TextBlock valueText;
-        protected List<Rectangle> valueRects;
-
-        private double graphBottom;
-        private double numTextLines;
+        protected Dictionary<ushort, BarColumn> PidDict;
 
         public VMultiBar(MainWindow mainWindow, MultiBarSettings panelSettings)
-            :base(mainWindow, panelSettings)
+            : base(mainWindow, panelSettings)
         {
-        }
-
-        private void RefreshChildren()
-        {
-            canvas.Children.RemoveRange(0, canvas.Children.Count);
-            titleText = new TextBlock();
-            valueText = new TextBlock();
-            foreach (var pid in ((MultiBarSettings)panelSettings).PIDList)
-            {
-                Rectangle valueRect = new Rectangle();
-                valueRect.Width = canvas.Width / ((MultiBarSettings)panelSettings).numPids;
-                valueRects.Add(valueRect);
-                canvas.Children.Add(valueRect);
-            }
-        }
-
-        protected override VMSDialog GenerateDlg()
-        {
-            return new MultiBarDlg(panelSettings);
+            PidDict = new Dictionary<ushort, BarColumn>();
         }
 
         public override void Init()
@@ -57,14 +35,96 @@ namespace VMSpc.Panels
         public override void GeneratePanel()
         {
             RefreshChildren();
-            numTextLines = Convert.ToInt32(((MultiBarSettings)panelSettings).showValue) + Convert.ToInt32(((MultiBarSettings)panelSettings).showName);
+        }
 
+        private void RefreshChildren()
+        {
+            canvas.Children.Clear();
+            PidDict.Clear();
+            foreach (ushort pid in ((MultiBarSettings)panelSettings).PIDList)
+            {
+                PidDict.Add(pid, new BarColumn(
+                                                canvas,
+                                                pid,
+                                                ((MultiBarSettings)panelSettings).numPids,
+                                                panelSettings.showInMetric,
+                                                ((MultiBarSettings)panelSettings).showGraph,
+                                                ((MultiBarSettings)panelSettings).showName,
+                                                ((MultiBarSettings)panelSettings).showValue,
+                                                ((MultiBarSettings)panelSettings).showUnit
+                                              ));
+            }
+        }
+
+        protected override VMSDialog GenerateDlg()
+        {
+            return new MultiBarDlg(panelSettings);
         }
 
         public override void UpdatePanel()
         {
-            throw new NotImplementedException();
+            foreach (var barColumn in PidDict.Values)
+            {
+                barColumn.Update();
+            }
+        }
+    }
+
+    public class BarColumn
+    {
+        private bool showGraph,
+                     showName,
+                     showValue,
+                     showUnit;
+
+        private TextBlock titleText;
+        private TextBlock valueText;
+        private Rectangle valueRect;
+        private ParamPresenter presenter;
+        private Canvas canvas;
+
+        private double graphBottom;
+        private double numTextLines;
+        private double width;
+
+        public BarColumn(Canvas canvas, ushort pid, int numPids, bool useMetric, bool showGraph, bool showName, bool showValue, bool showUnit)
+        {
+            this.showGraph = showGraph;
+            this.showName = showName;
+            this.showValue = showValue;
+            this.showUnit = showUnit;
+            width = (canvas.Width / numPids);
+
+            titleText = new TextBlock();
+            valueText = new TextBlock();
+            valueRect = new Rectangle();
+            presenter = new ParamPresenter(pid, useMetric);
+            numTextLines = (Convert.ToInt32(showValue) + Convert.ToInt32(showName || showUnit)) + 1;
+            graphBottom = canvas.Height / numTextLines;
+            graphBottom = (2 + numTextLines) * (canvas.Height / 4);
+            Draw();
         }
 
+        private void Draw()
+        {
+            if (showGraph)
+            {
+                canvas.Children.Add(valueRect);
+                Canvas.SetBottom(valueRect, graphBottom);
+            }
+            if (showName)
+            {
+                canvas.Children.Add(titleText);
+            }
+            if (showValue || showUnit)
+            {
+                canvas.Children.Add(valueText);
+            }
+        }
+
+        public void Update()
+        {
+
+        }
     }
 }
